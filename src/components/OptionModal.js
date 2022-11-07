129a91eca765423308a6b255afc4b2e6a1790cce
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-07 18:17:44 +0200

    styling application

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-  <Modal isOpen={!!props.selectedOption} onRequestClose={props.handleClearSelectedOption} contentLabel="Selected option">
-    <h3>Selected option: </h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
+  <Modal isOpen={!!props.selectedOption} onRequestClose={props.handleClearSelectedOption} contentLabel="Selected option" closeTimeoutMS={200} className="modal">
+    <h3 className='modal__title'>Selected option: </h3>
+    {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
     <button onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 );
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,9 +3,12 @@ import Option from "./Option";
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
-    {props.options.length === 0 && <p>Please add option to get started!</p>}
-    {props.options.map((option) => <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} />)}
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your options:</h3>
+      <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
+    </div>
+    {props.options.length === 0 && <p className="widget-message">Please add option to get started!</p>}
+    {props.options.map((option, index) => <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} count={index + 1} />)}
   </div>
 )
 
