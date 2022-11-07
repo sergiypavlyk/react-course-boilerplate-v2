90680f5e7c6ba3328813c447f43314be4892bfb5
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-07 14:28:49 +0200

    add sass loader

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<IndecisionApp />);
