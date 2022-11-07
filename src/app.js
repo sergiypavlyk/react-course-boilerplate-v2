29a833c8cdb19672ef82844276b9a7960a69e50e
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-07 20:07:42 +0200

    push existing app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<IndecisionApp />);
+root.render(<p>this is my boilerplate</p>);
