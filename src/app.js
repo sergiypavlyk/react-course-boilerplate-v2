fedd797831328b44a706032d769114a1f68c4996
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-08 17:59:36 +0200

    add list filters

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -10,9 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }))
-store.dispatch(addExpense({ description: 'Gas bill' }))
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
@@ -20,5 +21,11 @@ console.log(visibleExpenses);
 
 console.log(store.getState());
 
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
 const root = ReactDOMClient.createRoot(document.getElementById("app"));
-root.render(<AppRouter />);
+root.render(jsx);
