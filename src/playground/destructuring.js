5871f43d34875694cdcdc84d7815ad93dc04db71
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-08 13:01:10 +0200

    add redux and playground destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,43 @@
+const person = {
+  name: 'Sergiy',
+  age: 25,
+  location: {
+    city: 'NY',
+    temp: 92,
+  }
+};
+
+const { age, personName: firstName = 'Anonymous' } = person;
+// const name = person.name;
+// const age = person.age;
+
+console.log(`${firstName} is ${age}.`);
+
+const { city, temp: temperature } = person.location;
+if (city && temperature) {
+  console.log(`It's ${temperature} in ${city}.`);
+
+}
+
+const book = {
+  title: 'title',
+  author: 'author',
+  publisher: {
+    name: 'PublisherName'
+  }
+}
+
+const { name: publisherName = 'Self' } = book.publisher;
+console.log(publisherName);
+
+// array destructuring
+
+// const address = ['1213 address', 'NY', 'A', '1314'];
+// const [, symbol, state = 'New York'] = address;
+// console.log(`You are in ${symbol} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
+
