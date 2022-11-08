e7bbdf891b75b40143488266e086b35e949f243b
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-08 13:14:42 +0200

    add actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,33 @@
 import { createStore } from 'redux';
 
+// Action generators --> return action object
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+})
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+})
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+})
+
+const resetCount = () => ({ type: 'RESET' })
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
     return {
-      count: state.count + incrementBy
+      count: state.count + action.incrementBy
     }
     case 'DECREMENT':
-    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
     return {
-      count: state.count - decrementBy
+      count: state.count - action.decrementBy
     }
     case 'SET':
     return {
@@ -18,7 +35,7 @@ const store = createStore((state = { count: 0 }, action) => {
     }
     case 'RESET':
     return {
-      count: 0
+      count: action.resetCount
     }
     default: return state;
   }
@@ -26,27 +43,33 @@ const store = createStore((state = { count: 0 }, action) => {
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy: 5
+// });
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount());
 
 store.dispatch({
   type: 'DECREMENT'
 });
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
+// store.dispatch({
+//   type: 'DECREMENT',
+//   decrementBy: 10
+// });
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'RESET'
-});
+// store.dispatch({
+//   type: 'RESET'
+// });
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-})
+// store.dispatch({
+//   type: 'SET',
+//   count: 101
+// })
+store.dispatch(setCount({ count: 101 }));
 
 
