7905fc19a83da354bbb7c47adaf2119953295b09
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-08 23:13:29 +0200

    refactor code

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
+import { useNavigate } from 'react-router-dom';
 
-const AddExpensePage = () => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm />
-  </div>
-)
 
-export default AddExpensePage;
+const AddExpensePage = (props) => {
+  let navigate = useNavigate();
+  return (
+    <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm
+        onSubmit={(expense) => {
+          props.dispatch(addExpense(expense));
+          navigate('/');
+        }}
+      />
+    </div>
+  );
+}
+export default connect()(AddExpensePage);
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
+import { useParams, useNavigate } from 'react-router-dom';
 
-const EditExpensePage = () => (
-  <div>
-    Editing Expanse
-  </div>
-)
+const EditExpensePage = (props) => {
+  const { id } = useParams();
+  console.log("EditExpensePage ~ id", id)
+  let navigate = useNavigate();
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          navigate('/');
+        }}
+      />
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }));
+          navigate('/');
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  const params = useParams();
+  return {
+    expense: state.expenses.find((expense) => expense.id === params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
