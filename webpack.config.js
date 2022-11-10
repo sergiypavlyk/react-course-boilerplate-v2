02a9d8d38558ac37093edeaa6c7d14ee45f4d54f
sergiypavlyk <88386182+sergiypavlyk@users.noreply.github.com>
2022-11-10 21:01:42 +0200

    envs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,16 @@
 const path = require('path');
+const webpack = require('webpack');
+const { env } = require('process');
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+env.NODE_ENV = env.NODE_ENV || 'development';
+
+if (env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' });
+} else if (env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -34,7 +45,17 @@ module.exports = (env) => {
         ],
       }]
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin({
+        'env.FIREBASE_API_KEY': JSON.stringify(env.FIREBASE_API_KEY),
+        '.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(env.FIREBASE_AUTH_DOMAIN),
+        'env.FIREBASE_DATABASE_URL': JSON.stringify(env.FIREBASE_DATABASE_URL),
+        'env.FIREBASE_PROJECT_ID': JSON.stringify(env.FIREBASE_PROJECT_ID),
+        'env.FIREBASE_STORAGE_BUCKET': JSON.stringify(env.FIREBASE_STORAGE_BUCKET),
+        'env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.FIREBASE_MESSAGING_SENDER_ID)
+      })
+    ],
     devtool: isProduction? 'source-map' : 'inline-source-map',
     devServer: {
       static: path.join(__dirname, 'public'),
